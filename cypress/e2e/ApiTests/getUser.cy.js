a8956ca5a3760a4eea17965890c928e8cd03c877
Christian Rico Magat <Christianrico.Magat@sprint.com>
2022-10-25 06:52:21 +0800

    added fixtures

--- cypress/e2e/ApiTests/getUser.cy.js
+++ cypress/e2e/ApiTests/getUser.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+const createUser = require("../../fixtures/createUser")
+
 describe('TC001 - Get API user', () => {
 
     let accessToken = 'c8d653b9501e56011bf66070c208c44065bd787ef20f69d84617f2c4c0388a0d'
@@ -20,24 +22,24 @@ describe('TC001 - Get API user', () => {
         })
     })
 
-    it('TC002 - GET user info by user ID 3985', () => {
+    // it('TC002 - GET user info by user ID 3985', () => {
 
-        cy.request({
-            method: 'GET',
-            url: 'https://gorest.co.in/public/v2/users/3985',
-            headers: {
-                'authorization': "Bearer" + accessToken
-            }
+    //     cy.request({
+    //         method: 'GET',
+    //         url: 'https://gorest.co.in/public/v2/users/3985',
+    //         headers: {
+    //             'authorization': "Bearer" + accessToken
+    //         }
 
-        }).then((res) => {
-            expect(res.status).to.eq(200)
-            expect(res.body.name).to.eq('Meghnad Trivedi')
-            expect(res.body.email).to.eq('meghnad_trivedi@marvin-farrell.io')
-            expect(res.body.status).to.eq('active')
-        })
-    })
+    //     }).then((res) => {
+    //         expect(res.status).to.eq(200)
+    //         expect(res.body.name).to.eq('Meghnad Trivedi')
+    //         expect(res.body.email).to.eq('meghnad_trivedi@marvin-farrell.io')
+    //         expect(res.body.status).to.eq('active')
+    //     })
+    // })
 
-    it.only('TC003 - POST user info', () => {
+    it('TC002 - POST user info', () => {
 
         var random = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
         for (var i = 0; i < 10; i++)
@@ -53,21 +55,20 @@ describe('TC001 - Get API user', () => {
             },
             body: {
                 "email": testEmail,
-                "name": "API test automation",
-                "gender": "male",
-                "status": "active"
+                "name": createUser.name,
+                "gender": createUser.gender,
+                "status": createUser.status
             }
          }).then((res) => {
             cy.log(JSON.stringify(res))
             expect(res.status).to.eq(201)
-            expect(res.body).has.property('name', 'API test automation')
+            expect(res.body).has.property('name', createUser.name)
             expect(res.body).has.property('email', testEmail)
-            expect(res.body).has.property('gender', 'male')
-            expect(res.body).has.property('status', 'active')
+            expect(res.body).has.property('gender', createUser.gender)
+            expect(res.body).has.property('status', createUser.status)
 
         })
 
     })
     
-
 })
